c1abc6136afb4f36064205616e5ca83838d9e189
Zeeshan <zeeshan@guiker.com>
2020-06-30 17:24:34 +0530

    base set up

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap/dist/css/bootstrap.css";
 import App from './App.vue'
+import store from './store'
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app')
